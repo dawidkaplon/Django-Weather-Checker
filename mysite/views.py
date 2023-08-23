37e5b2d36a30dbe6c0540be6e72aa5823901f29e
Dawid Kapłon <dawidkaplon2@gmail.com>
2023-08-23 19:43:10 +0200

    Add better weather icons

--- mysite/views.py
+++ mysite/views.py
@@ -20,19 +20,40 @@ def index(request):
 
             return render(request, 'index.html', context)
         except KeyError:
-            return render(request, 'key_error.html')
+            return render(request, 'index.html', {'error': 'There is no such city!'})
     else:
         return render(request, 'index.html')
 
 
 def fetch_weather(city, api_key, current_weather_url):
     response = requests.get(current_weather_url.format(city, api_key)).json()
+    description = response['weather'][0]['description']
 
+    if 'clear' in description:
+        icon_src = 'https://i.ibb.co/TrJ8RhK/sun.png'
+    elif 'clouds' in description:
+        icon_src = 'https://i.ibb.co/C1QZ55c/clouds.png'
+    elif 'rain' in description:
+        icon_src = 'https://i.ibb.co/MV8VFLv/rain.png'
+    elif 'thunderstorm' in description:
+        icon_src = 'https://i.ibb.co/h7rp53z/thunderstorm.png'
+    elif 'snow' in description:
+        icon_src = 'https://i.ibb.co/rc5mHb1/snow.png'
+    elif 'mist' in description:
+        icon_src = 'https://i.ibb.co/wwFBTfS/mist.png'
+        
     weather_data = {
         'city': city,
-        'temperature': round(response['main']['temp'] - 273.15, 2),
-        'description': response['weather'][0]['description'],
-        'icon': response['weather'][0]['icon'],
+        'temperature': round(response['main']['temp'] - 273.15, 1),
+        'description': description,
+        'icon': icon_src,
+        'country': response['sys']['country'],
+        'feel_temp': round(response['main']['feels_like'] - 273.15, 1),
+        'min_temp': round(response['main']['temp_min'] - 273.15, 1),
+        'max_temp': round(response['main']['temp_max'] - 273.15, 1),
+        'wind_speed': response['wind']['speed'],
+        'pressure': response['main']['pressure'],
+        'humidity': response['main']['humidity'],
     }
 
     return weather_data
